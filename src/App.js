fd6e725e7f49ab95f8d2f016b41d6ddc9baa61a6
Anantshah133 <anantshah130305@gmail.com>
2023-08-26 18:49:39 +0530

    Added Main Banner

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Navbar from './web-components/navbar';
+import MainBanner from './web-components/main-banner';
 
 function App() {
   return (
-    <Navbar/>
+    <>
+      <Navbar/>
+      <MainBanner/>
+    </>
   )
 }
 
---
+++ src/web-components/main-banner.jsx
@@ -0,0 +1,17 @@
+function MainBanner(){
+    return(
+        <div className="main-banner-wrapper d-flex align-items-center justify-content-center">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12">
+                        <div className="inner text-center">
+                            <h1 class="title theme-gradient">Creative</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MainBanner;
